6ddd7f280f774e43ca204e126349ad26a5cc2feb
Sameep Kathayat <73783530+sameepkat@users.noreply.github.com>
2026-06-24 18:46:33 +0545

    Solve rnn-step-forward

---
+++ rnn-step-forward/rnn-step-forward.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
+	x_t = np.asarray(x_t)
+	h_prev = np.asarray(h_prev)
+	Wx = np.asarray(Wx)
+	Wh = np.asarray(Wh)
+	b = np.asarray(b)
+	
+	pre_act = x_t @ Wx + h_prev @ Wh + b
+	h_t = np.tanh(pre_act)
+	return h_t
